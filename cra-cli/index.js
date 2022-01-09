f54693136b0d188c109dd02637e5399983358ba6
nbili <coding520@163.com>
2022-01-09 21:37:02 +0800

    refactor

--- cra-cli/index.js
+++ cra-cli/index.js
@@ -4,16 +4,21 @@ const path = require("path");
 const copy = require("copy-template-dir");
 
 const vars = {
-  name: "nbili",
+  name: "cli-image",
+  version: "0.0.1",
+  description: "create a cli",
 };
 
 const inDir = path.join(__dirname, `template`);
-const outDir = path.join(process.cwd(), `output`);
+const outDir = path.join(process.cwd(), vars.name);
 
 copy(inDir, outDir, vars, (err, createdFile) => {
   if (err) throw err;
 
   createdFile.forEach((filePath) => {
-    console.log(`created ${filePath}`);
+    const fileName = path.basename(filePath);
+    console.log(`Created: ${fileName}`);
   });
+
+  console.log('Done!')
 });
