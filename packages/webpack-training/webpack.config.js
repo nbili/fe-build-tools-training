3d95dfc62cdd3d3758c8c1fe2994cbd6a3964219
nbili <coding520@163.com>
2022-02-21 00:32:00 +0800

    build

--- packages/webpack-training/webpack.config.js
+++ packages/webpack-training/webpack.config.js
@@ -1,18 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const resolve = path.resolve;
+
 module.exports = {
   mode: "development",
   // mode: "production",
   entry: "./src/index.js",
   output: {
+    path: resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash:5].js",
+    chunkFilename: "[name].[chunkhash:5].js",
     clean: true,
-    library: {
-      // name: "Test",
-      // libraryTarget: "commonjs",
-      type: "module",
-      umdNamedDefine: true,
-    },
+    publicPath: "/",
+    // library: {
+    //   // name: "Test",
+    //   libraryTarget: "umd",
+    //   // type: "module",
+    //   // umdNamedDefine: true,
+    // },
   },
-	externals: ['Test'],
+  devServer: {
+    contentBase: resolve(__dirname, "dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+  },
   experiments: {
     outputModule: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `./public/index.html`,
+      filename: `index.html`,
+    }),
+  ],
 };
